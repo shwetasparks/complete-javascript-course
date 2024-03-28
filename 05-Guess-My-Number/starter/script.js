6a0c00b82e5efab1397aeebc8b93cc16a80018dd
Shweta <shwetakoffficial@gmail.com>
2024-03-28 17:35:56 +0530

    guess the number

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(secretNumber);
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').innerContent = '😤Not a number';
+
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width='30rem';
+
+
+  } else if (guess > secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = '📈Too high';
+      score = score - 1;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '😥You lost';
+    }
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '📉Too low';
+    score = score - 1;
+  }
+});
